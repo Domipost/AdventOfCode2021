5809a90ebeca7173c1dba364fb9e645dc57e856c
Dominick Vietor <Dominick.Vietor@rabobank.com>
2021-12-13 18:10:49 +0100

    Day One Question 1

---
+++ AdventOfCode 2021/Classes/DayOne.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace AdventOfCode_2021.Classes
+{
+    public class DayOne
+    {
+        public static List<string> GetInput()
+        {
+            string input = @"C:\Users\Dominick\source\repos\AdventOfCode2021\AdventOfCode 2021\InputFiles\DayOne.txt";
+
+            return File.ReadAllLines(input).ToList();
+        }
+
+        public static int Answer1()
+        {
+            var input = GetInput();
+
+            int counter = 0;
+
+            for (int i = 0; i < input.Count - 1; i++)
+            {
+                int currentLine = int.Parse(input[i]);
+                int nextLine = int.Parse(input[i + 1]);
+
+                if (currentLine < nextLine)
+                {
+                    counter++;
+                }
+
+            }
+
+            return counter;
+        }
+
+        public static int Answer2()
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode 2021/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode_2021.Classes;
+
+Console.WriteLine("The answer of DayOne is: " + DayOne.Answer1());
+Console.WriteLine("The answer of DayOne is: " + DayOne.Answer2());
